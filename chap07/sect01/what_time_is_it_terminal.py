368bd6583a51cb497344edb37f53f0198784412f
narahub123 <parknara0213@gmail.com>
2026-02-15 17:35:03 +0900

    feat: chap07 sec01 현재 시간을 알아내는 펑션 콜링 예시 추가

---
+++ chap07/sect01/what_time_is_it_terminal.py
@@ -0,0 +1,54 @@
+from gpt_functions import get_current_time, tools
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+api_key = os.getenv("OPENAI_API_KEY")
+
+client= OpenAI(api_key=api_key)
+
+def get_ai_response(messages, tools=None):
+    response = client.chat.completions.create(
+        model="gpt-4o", 
+        messages=messages,
+        tools=tools
+    )
+
+    return response
+
+messages = [
+    {"role": "system", "content": "너는 사용자를 도와주는 상담사야"}
+] # 초기 시스템 메시지 설정
+
+while True:
+    user_input = input("사용자: ")
+
+    if user_input == "exit":
+        break
+
+    messages.append({"role": "user", "content": user_input})
+
+    ai_response = get_ai_response(messages, tools=tools)
+    ai_message = ai_response.choices[0].message
+    print(ai_message)
+
+    tool_calls = ai_message.tool_calls
+    if tool_calls: # toole_calls가 존재하는 경우
+        tool_name = tool_calls[0].function.name
+        tool_call_id = tool_calls[0].id
+
+        if tool_name == "get_current_time":
+            messages.append({
+                "role": "function", # role을 function으로 설정
+                "tool_call_id": tool_call_id,
+                "name": tool_name,
+                "content": get_current_time() # 함수 실행 결과를 content로 설정
+            })
+
+        ai_response = get_ai_response(messages, tools=tools)
+        ai_message = ai_response.choices[0].message
+
+    messages.append(ai_message) # AI 응답을 메시지에 추가
+
+    print("AI\t: " + ai_message.content) # AI 응답 출력

62de460a346be3afc55eb9eb758b840e7cdf6324
narahub123 <parknara0213@gmail.com>
2026-02-23 14:34:58 +0900

    feat: chap14 sec01 콘텐츠 전략가 에이전트 만들기

--- chap14/sect01/book_writer.py
+++ chap14/sect01/book_writer.py
@@ -1,11 +1,12 @@
 from langgraph.graph import StateGraph, START, END
 from langchain_openai import ChatOpenAI
-from langchain_core.messages import AnyMessage, SystemMessage, HumanMessage
+from langchain_core.messages import AnyMessage, SystemMessage, HumanMessage, AIMessage
 from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers.string import StrOutputParser
 from typing_extensions import TypedDict
 from typing import List
 
-from utils import save_state # 나중에 작성 예정 
+from utils import save_state, get_outline, save_outline
 
 from datetime import datetime 
 
@@ -28,6 +29,53 @@ llm = ChatOpenAI(model="gpt-4o")
 class State(TypedDict):
     messages: List[AnyMessage|str]
 
+# 목차를 작성하는 노드 : content_strategist
+def content_strategist (state: State):
+    print("\n\n==============CONTENT STRATEGIST================")
+
+    content_strategist_system_prompt = PromptTemplate.from_template(
+        """
+        너는 책을 쓰는 AI 팀의 콘텐츠 전략가(Content Strategist)로서, 
+        이전 대화 내용을 바탕으로 사용자의 요구 사항을 분석하고, AI팀이 쓸 책의 세부 목차를 결정한다.
+
+        지난 목차가 있다면 그 버전을 사용자의 요구에 맞게 수정하고, 없다면 새로운 목차를 제안한다.
+        
+        ---------------------------
+        - 지난 목차 : {outline}
+        ---------------------------
+        - 이전 대화 내용 : {messages}
+        """
+    )
+
+    content_strategist_chain = content_strategist_system_prompt | llm | StrOutputParser()
+
+    #  상태 메시지 가져오기 
+    messages = state['messages']
+    # 저장된 목차 가져오기 
+    outline = get_outline(current_path)
+
+    # 입력값 정의 
+    inputs = {
+        'messages': messages, 
+        "outline": outline
+    }
+
+    gathered = ''
+
+    for chunk in content_strategist_chain.stream(inputs):
+        gathered += chunk
+        print(chunk, end="")
+
+    print()
+
+    save_outline(current_path, gathered)
+
+    content_strategist_message = f"[Content Strategist] 목차 작성 완료"
+    print(content_strategist_message)
+    messages.append(AIMessage(content_strategist_message))
+
+    return {'messages': messages}
+
 # 사용자와 대화할 노드(agent) : communicator 
 def communicator(state: State):
     print("\n\n==============COMMUNICATOR================")
@@ -36,6 +84,8 @@ def communicator(state: State):
         """
         너는 책을 쓰는 AI 팀의 커뮤니케이터로서, 
         AI 팀의 진행 상황을 사용자에게 보고하고, 사용자의 의견을 파악하기 위해 대화를 나눈다.
+
+        사용자도 outline(목차)을 이미 보고 있으므로, 다시 출력할 필요없다.
 
         messages: {messages}
         """
@@ -69,9 +119,11 @@ graph_builder = StateGraph(State)
 
 # Nodes 
 graph_builder.add_node("communicator", communicator)
+graph_builder.add_node("content_strategist", content_strategist)
 
 # Edges 
-graph_builder.add_edge(START, 'communicator')
+graph_builder.add_edge(START, 'content_strategist')
+graph_builder.add_edge('content_strategist', 'communicator')
 graph_builder.add_edge('communicator', END)
 
 graph = graph_builder.compile()
